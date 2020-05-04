b1e097bee1942ffbf2830fc032dd38d464c8590e
mndgit <mndhome@outlook.com>
2020-05-04 18:42:39 -0400

    add files

---
+++ extract.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from splinter import Browser
+from bs4 import BeautifulSoup
+import requests
+
+def scrape():
+    #url to be scraped
+    url = 'https://www.heritage.org/index/ranking'
+
+    ########################################################
+    # splinter method
+    ########################################################
+    #use chrome as browser and visit url/parse html from url
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+    browser.visit(url)
+    html = browser.html
+    splinter_soup = BeautifulSoup(html, 'html.parser')
+
+    ########################################################
+    # request method
+    ########################################################
+    #retrieve page and parse html from url
+    response = requests.get(url)
+    request_soup = BeautifulSoup(response.text, 'html.parser')
+    #find all table data
+    results = request_soup.find_all('td')
+
+    splinter_tables = splinter_soup.find_all("td")
+    request_tables = request_soup.find_all("td")
+
+    assert (splinter_tables == request_tables)
+
+    #create empty lists
+    country_list, overall_list, change_list = ([] for i in range(3))
+
+    #append result to a list depending on the td class is associated with
+    for result in results:
+        text = result.text.strip()
+        if 'country' in result['class']:
+            country_list.append(text)
+        elif 'overall' in result['class']:
+            overall_list.append(text)
+        elif 'change' in result['class']:
+            change_list.append(text)
+
+    #convert to dataframe
+    dict = {'country': country_list,'overall': overall_list,'change': change_list}
+    rankings_df = pd.DataFrame(dict)
+
+    return rankings_df
---
+++ transform.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import config
+from sqlalchemy import create_engine
+import extract
+
+def transform():
+    #extract data
+    rankings_df = extract.scrape()
+
+    #reading data from csv file
+    file_to_load = "Resources/countries_by_population_2019.csv"
+    country_population = pd.read_csv(file_to_load)
+
+    #connection made to connect to Postgres sql to bring the country_code table from country Database(Data#2 from SQL DB)
+    engine = create_engine(f'postgresql://{config.postgres_conn_str}{config.csv_database}')
+    connection = engine.connect()
+    #reading data from sql DB and merging with CSV data
+    country_code=pd.read_sql("SELECT * FROM country_code", connection)
+    country_code_population = country_population.merge(country_code, on='name')
+
+    #drop NA
+    country_code_population=country_code_population.dropna(how="all",axis=0)
+    country_code_population=country_code_population.dropna(how="all",axis=1)
+    country_code_population=country_code_population
+
+    #formating thevalues
+    country_code_population['Density']=country_code_population['Density'].map("{0:.2f}".format)
+    country_code_population['pop2019']=country_code_population['pop2019'].map("{0:.0f}".format)
+    country_code_population['area']=country_code_population['area'].map("{0:.0f}".format)
+
+    #renaming  and eliminating columns
+    country_code_population = country_code_population.rename(columns={"name": "country",
+                                                "pop2019": "Population",
+                                                "area": "Area","Density":"Density","cca2":"Country Code_1","cca3":"Country Code_2","ccn3":"Country Code_#"
+                                                })
+    country_code_population=country_code_population[["Rank","country","Population","Area","Density","Country Code_1","Country Code_2","Country Code_#"]]
+
+    #combining the data with the webscraped data
+    rankings_country_code_population=country_code_population.merge(rankings_df, on='country')
+
+    #removing duplicates
+    rankings_country_code_population.drop_duplicates(subset ="country", keep = False, inplace = True)
+    rankings_country_code_population.head()
